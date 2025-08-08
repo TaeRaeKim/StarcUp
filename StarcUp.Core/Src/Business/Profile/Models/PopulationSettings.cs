10877bf3a6de474033c0e26440f9e405604b51cb
TaeRae Kim <dkfhdksp@gmail.com>
2025-08-09 03:39:02 +0900

    Feat: PopulationManager 및 프리셋 모델 기능 확장
    - PopulationSettings 모델 속성 확장
    - PresetModels에 새로운 프리셋 타입 추가
    - PopulationManager 인구 부족 알림 시스템 개선
    - UnitTypeExtensions 유닛 타입 확장 메서드 추가

--- StarcUp.Core/Src/Business/Profile/Models/PopulationSettings.cs
+++ StarcUp.Core/Src/Business/Profile/Models/PopulationSettings.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using StarcUp.Business.Units.Types;
 
 namespace StarcUp.Business.Profile.Models
@@ -9,14 +10,20 @@ namespace StarcUp.Business.Profile.Models
     /// </summary>
     public class PopulationSettings
     {
+        [JsonPropertyName("mode")]
         public PopulationMode Mode { get; set; } = PopulationMode.Fixed;
+        
+        [JsonPropertyName("fixedSettings")]
         public FixedModeSettings? FixedSettings { get; set; }
+        
+        [JsonPropertyName("buildingSettings")]
         public BuildingModeSettings? BuildingSettings { get; set; }
     }
 
     /// <summary>
     /// 인구수 경고 모드
     /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum PopulationMode
     {
         Fixed,   // 모드 A: 고정값 기반
@@ -28,7 +35,10 @@ namespace StarcUp.Business.Profile.Models
     /// </summary>
     public class FixedModeSettings
     {
+        [JsonPropertyName("thresholdValue")]
         public int ThresholdValue { get; set; } = 4;  // 1-50 범위
+        
+        [JsonPropertyName("timeLimit")]
         public TimeLimitSettings? TimeLimit { get; set; }
     }
 
@@ -37,9 +47,15 @@ namespace StarcUp.Business.Profile.Models
     /// </summary>
     public class TimeLimitSettings
     {
+        [JsonPropertyName("enabled")]
         public bool Enabled { get; set; } = true;
+        
+        [JsonPropertyName("minutes")]
         public int Minutes { get; set; } = 3;    // 0-59
+        
+        [JsonPropertyName("seconds")]
         public int Seconds { get; set; } = 0;    // 0-59
+        
         public int TotalSeconds => Minutes * 60 + Seconds;
     }
 
@@ -48,7 +64,10 @@ namespace StarcUp.Business.Profile.Models
     /// </summary>
     public class BuildingModeSettings
     {
+        [JsonPropertyName("race")]
         public RaceType Race { get; set; } = RaceType.Protoss;
+        
+        [JsonPropertyName("trackedBuildings")]
         public List<TrackedBuilding> TrackedBuildings { get; set; } = new();
     }
 
@@ -57,9 +76,13 @@ namespace StarcUp.Business.Profile.Models
     /// </summary>
     public class TrackedBuilding
     {
+        [JsonPropertyName("buildingType")]
         public UnitType BuildingType { get; set; } = UnitType.ProtossGateway;
-        public string Name { get; set; } = "";
+        
+        [JsonPropertyName("multiplier")]
         public int Multiplier { get; set; } = 1;  // 1-10 범위
+        
+        [JsonPropertyName("enabled")]
         public bool Enabled { get; set; } = false;
     }
 
--- StarcUp.Core/Src/Business/Units/Types/UnitTypeExtensions.cs
+++ StarcUp.Core/Src/Business/Units/Types/UnitTypeExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
+using System.Text.Json.Serialization;
 
 namespace StarcUp.Business.Units.Types
 {
